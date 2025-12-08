bb2229439ac86a59dbf0048e3b34dc564ea1e255
RustyNova <50844553+RustyNova016@users.noreply.github.com>
2025-12-08 10:20:16 +0000

    chore: move LinkItem

--- src/components/link_category.tsx
+++ src/components/link_category.tsx
@@ -5,6 +5,7 @@ import { IArtistCredit } from "musicbrainz-api";
 import { artist_credits } from "@/mb_fetching";
 import { link_domains } from "@/globals";
 import { UrlData } from "@/models/url";
+import { LinkItem } from "./link_list/link/link";
 
 export function LinkCategory({
     data,
@@ -27,78 +28,10 @@ export function LinkCategory({
             <h4 title={hover}>{title}</h4>
             <>
                 {url_groups.map((link_group, key) => {
-                    return <LinkPill key={key} links={link_group}></LinkPill>;
+                    return <LinkItem key={key} links={link_group}></LinkItem>;
                 })}
             </>
         </div>
     );
 }
 
-function LinkPill({ links }: { links: UrlData[] }) {
-    let main_link = links.pop();
-    let alt_links = links.length > 0;
-
-    return (
-        <>
-            <div className={`btn-group ${styles.link_button_group}`}>
-                <MainLink link={main_link} has_alts={alt_links} />
-                {alt_links ? (
-                    <>
-                        <button
-                            type="button"
-                            className={`${styles.link_button} ${styles.link_button_right} dropdown-toggle dropdown-toggle-split`}
-                            data-bs-toggle="dropdown"
-                            aria-expanded="false"
-                        >
-                            <span className="visually-hidden">
-                                Toggle Dropdown
-                            </span>
-                        </button>
-                        <ul className="dropdown-menu">
-                            {links.map((alt_link, index) => (
-                                <AltLink
-                                    key={index}
-                                    index={index + 1}
-                                    link={alt_link}
-                                />
-                            ))}
-                        </ul>
-                    </>
-                ) : (
-                    <></>
-                )}
-            </div>
-        </>
-    );
-}
-
-function MainLink({ link, has_alts }: { link: UrlData; has_alts: boolean }) {
-    let button_class = has_alts
-        ? styles.link_button_left
-        : styles.link_button_solo;
-
-    return (
-        <Link
-            href={link.ressource}
-            className={`${styles.link_button} ${button_class}`}
-        >
-            {" "}
-            <div className={styles.link_icon}>
-                <img
-                    src={`http://www.google.com/s2/favicons?domain=${link.get_hostname()}`}
-                ></img>
-            </div>
-            <div className={styles.link_text}>{link.get_name()}</div>
-        </Link>
-    );
-}
-
-function AltLink({ link, index }: { link: UrlData; index: number }) {
-    return (
-        <li>
-            <Link className="dropdown-item" href={link.ressource}>
-                Alternate Link {index}
-            </Link>
-        </li>
-    );
-}
---
+++ src/components/link_list/link/link.tsx
@@ -0,0 +1,81 @@
+import { UrlData } from "@/models/url";
+import styles from "./link.module.scss";
+import Link from "next/link";
+
+export type LinkPillProps = {
+    links: UrlData[];
+};
+
+// A link in the link list
+export function LinkItem({ links }: LinkPillProps) {
+    let main_link = links.pop();
+    if (main_link === undefined) {
+        return <></>
+    }
+
+    let alt_links = links.length > 0;
+
+    return (
+        <>
+            <div className={`btn-group ${styles.link_button_group}`}>
+                <MainLink link={main_link} has_alts={alt_links} />
+                {alt_links ? (
+                    <>
+                        <button
+                            type="button"
+                            className={`${styles.link_button} ${styles.link_button_right} dropdown-toggle dropdown-toggle-split`}
+                            data-bs-toggle="dropdown"
+                            aria-expanded="false"
+                        >
+                            <span className="visually-hidden">
+                                Toggle Dropdown
+                            </span>
+                        </button>
+                        <ul className="dropdown-menu">
+                            {links.map((alt_link, index) => (
+                                <AltLink
+                                    key={index}
+                                    index={index + 1}
+                                    link={alt_link}
+                                />
+                            ))}
+                        </ul>
+                    </>
+                ) : (
+                    <></>
+                )}
+            </div>
+        </>
+    );
+}
+
+function MainLink({ link, has_alts }: { link: UrlData; has_alts: boolean; }) {
+    let button_class = has_alts
+        ? styles.link_button_left
+        : styles.link_button_solo;
+
+    return (
+        <Link
+            href={link.ressource}
+            className={`${styles.link_button} ${button_class}`}
+        >
+            {" "}
+            <div className={styles.link_icon}>
+                <img
+                    src={`http://www.google.com/s2/favicons?domain=${link.get_hostname()}`}
+                ></img>
+            </div>
+            <div className={styles.link_text}>{link.get_name()}</div>
+        </Link>
+    );
+}
+
+function AltLink({ link, index }: { link: UrlData; index: number; }) {
+    return (
+        <li>
+            <Link className="dropdown-item" href={link.ressource}>
+                Alternate Link {index}
+            </Link>
+        </li>
+    );
+}
