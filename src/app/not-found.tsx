147f63458c60bf9ccc07af3ee5f3123c161ea116
RustyNova <50844553+RustyNova016@users.noreply.github.com>
2025-12-08 10:44:28 +0000

    feat: context specific not found message

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,18 +1,11 @@
 import { PageModal } from "@/components/stateless/modal";
 import styles from "./not-found.module.scss";
+import { NotFoundPage } from "@/components/ui/pages/not_found";
 
 export default function NotFound() {
     return (
         <>
-            <PageModal>
-                <div className={`${styles.container}`}>
-                    <p className={`${styles.title}`}>404 :(</p>
-                    {/* <span className={`${styles.subtext}`}>
-                        <i>Record scratch</i>
-                    </span> */}
-                    <span>It doesn't seems like this page exists...</span>
-                </div>
-            </PageModal>
+            <NotFoundPage item_name="page"/>
         </>
     );
 }
